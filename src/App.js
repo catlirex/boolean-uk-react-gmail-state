bfe418b9f3ca8613f51d47b1a553fa5a062e458f
Catherine Li <catherine.uk.20@gmail.com>
2021-06-09 19:16:15 +0100

    add new email, updated renderEmail with reverse()

--- src/App.js
+++ src/App.js
@@ -15,12 +15,15 @@ function App() {
   const unreadCount = emails.filter(email => !email.read).length
   const starCount = emails.filter(email => email.starred).length
 
-  let emailToRender = emails
+  let emailToRender = [...emails].reverse()
+  console.log("emailToRender", emailToRender);
+  console.log("emails", emails);
   if(currentSection === "starred") emailToRender = emailToRender.filter(email=>email.starred)
   if(hideRead)emailToRender = emailToRender.filter(email=> !email.read)
   if(searchValue !== "") emailToRender = emailToRender.filter(email =>
     ['title', 'sender'].some(key => email[key].toLowerCase().includes(searchValue.toLowerCase())
   ))
+  
 
   // const emailToRender = currentSection === "inbox"? hideRead? emails.filter(email=> !email.read) : emails: hideRead? emails.filter(email=> email.starred && !email.read) : emails.filter(email=>email.starred)
 
@@ -91,7 +94,6 @@ function App() {
       </nav>
       <main className="emails">{
       <ul>
-      
        {emailToRender.map(email => (
         <li key={email.id} className={"email " + (email.read? 'read' : 'unread')}>
         <input id="read" type="checkbox" onChange={()=>updateRead(email)} checked={email.read}/>
@@ -104,35 +106,50 @@ function App() {
     </ul>
       }
       
-      <section class="email-form-container">
-        <div class="avatar"></div>
-        <form id="email-form" >
-          <header class="form-header">
+      <section className="email-form-container">
+        <div className="avatar"></div>
+        <form id="email-form" onSubmit={function(event){
+          event.preventDefault()
+          let newEmail={
+            id: emails[emails.length -1].id +1,
+            sender : event.target[0].value,
+            title: event.target[1].value,
+            starred: false,
+            read: false
+          }
+          setEmails([...emails, newEmail])
+        }}>
+          <header className="form-header">
             <div>
-            <img class="icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAABEElEQVR42mNgGLyAnzLtGQx5lGn/x1BAvvZ0hn8M/8k3IA2snWwDYNrJNCAVrp0sA5C1/2doY2AhTXsKinYQ/MZwiKGTwY+BnTztCPiSoZlBCr/2ZDzaIfAXwwIGEVzakwhqh8BXDGGUaIfA5ZghMocE7SC4m4EL1QAmhnkkGnGEgY9SI9aie4N0I7IoNeI7gw4hI2YzxDOUMnQzLGTYznARS0ztwoxQVCNQM5MEQzrDDoafKEY44jeiAGs5GcVwDa7iBLZkhTACV3ZmY6iHu0QTnxH4ygMNhkNgNVXYpSFG4C9QGBmaGP4znMIlDTKCcIk0heEfgwxuI9QIGsDEsIohm7Lqh42hktIKkGPg62AActUwumHce/0AAAAASUVORK5CYII=" alt="reply button"/>
-            <span class="sender-email">Freepik Company (no-reply@freepik.com)</span>
-          </div>
-          <div><img className="icon" src='https://www.gstatic.com/images/icons/material/system/1x/open_in_new_black_18dp.png' alt="expandIcon" /></div>
+            <img className="icon" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAABEElEQVR42mNgGLyAnzLtGQx5lGn/x1BAvvZ0hn8M/8k3IA2snWwDYNrJNCAVrp0sA5C1/2doY2AhTXsKinYQ/MZwiKGTwY+BnTztCPiSoZlBCr/2ZDzaIfAXwwIGEVzakwhqh8BXDGGUaIfA5ZghMocE7SC4m4EL1QAmhnkkGnGEgY9SI9aie4N0I7IoNeI7gw4hI2YzxDOUMnQzLGTYznARS0ztwoxQVCNQM5MEQzrDDoafKEY44jeiAGs5GcVwDa7iBLZkhTACV3ZmY6iHu0QTnxH4ygMNhkNgNVXYpSFG4C9QGBmaGP4znMIlDTKCcIk0heEfgwxuI9QIGsDEsIohm7Lqh42hktIKkGPg62AActUwumHce/0AAAAASUVORK5CYII=" alt="reply button"/>
+            <div className="email-info-container">
+              <label htmlFor="sender">Sender: </label>
+              <input id="sender" className="sender-email" type='text' defaultValue='Freepik Company'/>
+              <label htmlFor="subject">Subject: </label>
+              <input id="subject" className="email-subject" type='text' defaultValue='RE: React is not that easy'/>
+            </div>
+            </div>
+          <img className="icon" src='https://www.gstatic.com/images/icons/material/system/1x/open_in_new_black_18dp.png' alt="expandIcon" />
           </header>
           <textarea name="email-reply" form="email-form" rows="5"></textarea>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/more_horiz_black_20dp.png"></img>
-        <div class="action-bar">
-            <div class="left-bar">
-            <button class="button">Send</button>
-          <img class="icon button" src="https://www.gstatic.com/images/icons/material/system/1x/arrow_drop_down_white_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/text_format_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/attach_file_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/insert_link_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/insert_emoticon_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/drive_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/insert_photo_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/lock_clock_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/ink_pen_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/more_horiz_black_20dp.png"></img>
+        <div className="action-bar">
+            <div className="left-bar">
+            <button className="button">Send</button>
+          <img className="icon button" src="https://www.gstatic.com/images/icons/material/system/1x/arrow_drop_down_white_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/text_format_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/attach_file_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/insert_link_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/insert_emoticon_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/drive_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/insert_photo_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/lock_clock_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/ink_pen_black_20dp.png"></img>
           </div>
 
-          <div class="right-bar">
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/more_vert_black_20dp.png"></img>
-          <img class="icon" src="https://www.gstatic.com/images/icons/material/system/1x/delete_black_20dp.png"></img> 
+          <div className="right-bar">
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/more_vert_black_20dp.png"></img>
+          <img className="icon" src="https://www.gstatic.com/images/icons/material/system/1x/delete_black_20dp.png"></img> 
           </div>
 
         </div>
